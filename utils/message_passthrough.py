d5ba717caf3c617c6935b8e48ad4d253fa1f1506
kalutep <petr.docekal.hdcr@gmail.com>
2022-03-24 10:55:20 +0100

    Initial commit

--- utils/message_passthrough.py
+++ utils/message_passthrough.py
@@ -1,10 +1,12 @@
 import serial
 
-#use this when connecting to a PC with diagnostic software
-#ser1 = serial.Serial('/dev/serial0', 2400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+# use this when connecting to a PC with diagnostic software
+# ser1 = serial.Serial('/dev/serial0', 2400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
 
-ser1 = serial.Serial('/dev/ttyUSB1', 2400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
-ser2 = serial.Serial('/dev/ttyUSB0', 2400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+ser1 = serial.Serial('/dev/ttyUSB1', 2400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
+                     stopbits=serial.STOPBITS_ONE)
+ser2 = serial.Serial('/dev/ttyUSB0', 2400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
+                     stopbits=serial.STOPBITS_ONE)
 
 ser1.flushInput()
 ser2.flushInput()
@@ -17,13 +19,12 @@ while True:
             print('1 >> 2: ERROR: ', message)
             continue
         message += ser1.read(2)
-        message += ser1.read(message[-1]+4)
+        message += ser1.read(message[-1] + 4)
 
         ser2.write(message)
-        
-        print('1 >> 2: ', (message).hex())
 
- 
+        print('1 >> 2: ', message.hex())
+
     if ser2.inWaiting() > 0:
         message = ser2.read(1)
         if message != b'\xaa':
@@ -31,11 +32,8 @@ while True:
             print('2 >> 1: ERROR: ', message)
             continue
         message += ser2.read(2)
-        message += ser2.read(message[-1]+4)
+        message += ser2.read(message[-1] + 4)
 
         ser1.write(message)
 
-        print('2 >> 1: ', (message).hex())
-
-        
-
+        print('2 >> 1: ', message.hex())
